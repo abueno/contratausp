a26305257be94bfb0e9ba4e7a00ee7900a4e3f27
Alberto Bueno Junior <alberto.bueno.jr@gmail.com>
2013-11-23 12:34:03 -0200

    contratausp/views.py

--- contratausp/views.py
+++ contratausp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template.context import RequestContext
+from django.shortcuts import render_to_response
 
 def home(request):
     return render_to_response('home.html', {}, RequestContext(request))
